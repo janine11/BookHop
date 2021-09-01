6bfb42912d7327280568c865ae8a9fce7a5cfcef
Janine Luongo <janineluongo@gmail.com>
2021-09-01 11:33:29 -0400

    set up routes and pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./pages/SearchPage"
+import FavoritesPage from "./pages/FavoritesPage"
+import HomePage from "./pages/HomePage"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App"></div>
+      <Switch>
+
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+
+        <Route path="/search">
+          <SearchPage />
+        </Route>
+
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+
+      </Switch>
+    </Router>
   );
 }
 
